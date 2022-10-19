24d39a54417c4f661185a806a834ca628a072087
HenriqueVidal <hpvidal@outlook.com>
2022-10-19 00:41:50 -0300

    CORREÇÕES E MUDANÇAS GERAIS

--- js/script.js
+++ js/script.js
@@ -3,12 +3,13 @@ const signals = document.querySelectorAll('dd');
 const clear = document.querySelector('#clear');
 let exit = document.querySelector('#exit');
 let values, operator = [], operation = [];
+const operators_regex = /[*+x/-\s]/; //   /[+-\s]/; CAPTURA OS CARACTERES + OU - OU / OU *
 
 // PEGA CADA CLICK E ADICIONA UM APÓS O OUTRO
-callback = (e) =>{
+const callback = (e) => {
     values = e.target.innerText;
     exit.value += values;
-}
+};
 
 //  EVENTO DE CLICK PARA CADA NUMERO
 numeral.forEach((i) => {
@@ -20,12 +21,14 @@ signals.forEach((i) => {
     operator.push(i);
 });
 
-// EVENTO DE CLICK AO OPERADOR +
+// EVENTO DE CLICK AO OPERADOR +, -, ÷, x
 // PEGA O VALOR TECLADO E ADICIONA NO INICIO DE UM ARRAY
 // EXITE O VALOR NO VISOR COM O OPERADOR SELECIONADO
-operator[5].addEventListener('click', () => {
-    operation.unshift(parseFloat(exit.value));
-    exit.value = `${operation[0]}+`;
+[operator[0], operator[1], operator[2], operator[5]].forEach(e=> {
+    e.addEventListener('click', (event) => {
+        console.log(event.target.innerText);    
+        exit.value += event.target.innerText;
+    });
 });
 
 // EVENTO DE CLICK AO OPERADOR =
@@ -34,10 +37,18 @@ operator[5].addEventListener('click', () => {
 // PEGA O VALOR DA SOMA E ADICIONA AO INICIO DO ARRAY
 // EXIBE O RESULTADO DA OPERAÇÃO NO VISOR
 operator[4].addEventListener('click', () => {
-    operation.unshift(parseFloat(exit.value));
-    soma = operation[0] + operation[1];
-    operation.unshift(soma);
-    exit.value = operation[0];
+    console.log(
+        exit.value.replaceAll("x", "*").replaceAll("÷", "/").split(operators_regex).toString(), 
+        exit.value.replaceAll("x", "*").replaceAll("÷", "/").split(operators_regex).length
+    );
+
+    if (exit.value.replaceAll("x", "*").replaceAll("÷", "/").split(operators_regex).length === 1) return; //    SE HOUVER APENAS UM CONJUNTO DE NUMERO FORA O OPERADOR, RETORNA
+
+    //const soma = exit.value.split(operators_regex).reduce((prev, curr)=> Number(prev) + Number(curr), 0); //    CORREÇÃO DO RESULTADO NaN
+    //operation.unshift(soma);
+
+    const soma = eval(exit.value.replaceAll("x", "*").replaceAll("÷", "/")); // ALTERAÇÃO DA FORMA DE SOMA
+    exit.value = soma; // CORREÇÃO RESULTADO
 });
 
 // LIMPA A TELA E OS ARRAYS PARA INICIAR UMA NOVA OPERAÇÃO
@@ -45,4 +56,4 @@ clear.addEventListener('click', () => {
     operation = [];
     operator = [];
     exit.value = '';
-})
+});
